190752fe7450e8eb1e7c64a311368c433d543a5c
Tanzim.Mokammel <mtanzim@gmail.com>
2021-01-25 00:03:14 -0500

    remove extra stuff

--- example/src/main/scala/Main.scala
+++
@@ -1,6 +0,0 @@
-import example.Lists
-
-object Main extends App {
-  println(Lists.sum(List(1, 4, 5, 6, 7, 7, 8, 8, 4, 45, 4545, 45, 45, 45, 454, 4, 545, 45, 33)))
-  println(Lists.max(List(1, 4, 5, 6, 7, 7, 8, 8, 4, 45, 4545, 45, 45, 45, 454, 4, 545, 45, 33)))
-}
