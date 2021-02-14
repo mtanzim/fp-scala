29f8ef743ea7046873a955fb3db09e9f3e586787
Tanzim.Mokammel <mtanzim@gmail.com>
2021-02-13 22:05:55 -0500

    additional solver setup

--- streams/src/main/scala/streams/GameDef.scala
+++ streams/src/main/scala/streams/GameDef.scala
@@ -15,16 +15,16 @@ trait GameDef {
    *
    * Illustration:
    *
-   *     0 1 2 3   <- col axis
-   *   0 o o o o
-   *   1 o o o o
-   *   2 o # o o    # is at position Pos(2, 1)
-   *   3 o o o o
+   * 0 1 2 3   <- col axis
+   * 0 o o o o
+   * 1 o o o o
+   * 2 o # o o    # is at position Pos(2, 1)
+   * 3 o o o o
    *
-   *   ^
-   *   |
+   * ^
+   * |
    *
-   *   row axis
+   * row axis
    */
   case class Pos(row: Int, col: Int) {
     /** The position obtained by changing the `row` coordinate by `d` */
@@ -70,16 +70,20 @@ trait GameDef {
    * These moves are encoded as case objects.
    */
   sealed abstract class Move
-  case object Left  extends Move
+
+  case object Left extends Move
+
   case object Right extends Move
-  case object Up    extends Move
-  case object Down  extends Move
+
+  case object Up extends Move
+
+  case object Down extends Move
 
   /**
    * This function returns the block at the start position of
    * the game.
    */
-  def startBlock: Block = ???
+  def startBlock: Block = Block(startPos, startPos)
 
 
   /**
@@ -106,46 +110,55 @@ trait GameDef {
 
 
     /** The block obtained by moving left */
-    def left = if (isStanding)             deltaCol(-2, -1)
-               else if (b1.row == b2.row)  deltaCol(-1, -2)
-               else                        deltaCol(-1, -1)
+    def left = if (isStanding) deltaCol(-2, -1)
+    else if (b1.row == b2.row) deltaCol(-1, -2)
+    else deltaCol(-1, -1)
 
     /** The block obtained by moving right */
-    def right = if (isStanding)            deltaCol(1, 2)
-                else if (b1.row == b2.row) deltaCol(2, 1)
-                else                       deltaCol(1, 1)
+    def right = if (isStanding) deltaCol(1, 2)
+    else if (b1.row == b2.row) deltaCol(2, 1)
+    else deltaCol(1, 1)
 
     /** The block obtained by moving up */
-    def up = if (isStanding)               deltaRow(-2, -1)
-             else if (b1.row == b2.row)    deltaRow(-1, -1)
-             else                          deltaRow(-1, -2)
+    def up = if (isStanding) deltaRow(-2, -1)
+    else if (b1.row == b2.row) deltaRow(-1, -1)
+    else deltaRow(-1, -2)
 
     /** The block obtained by moving down */
-    def down = if (isStanding)             deltaRow(1, 2)
-               else if (b1.row == b2.row)  deltaRow(1, 1)
-               else                        deltaRow(2, 1)
+    def down = if (isStanding) deltaRow(1, 2)
+    else if (b1.row == b2.row) deltaRow(1, 1)
+    else deltaRow(2, 1)
 
 
     /**
      * Returns the list of blocks that can be obtained by moving
      * the current block, together with the corresponding move.
      */
-    def neighbors: List[(Block, Move)] = ???
+    def neighbors: List[(Block, Move)] =
+      List((left, Left)
+        ,
+        (right, Right)
+        ,
+        (up, Up)
+        ,
+        (down, Down)
+      )
 
     /**
      * Returns the list of positions reachable from the current block
      * which are inside the terrain.
      */
-    def legalNeighbors: List[(Block, Move)] = ???
+    def legalNeighbors: List[(Block, Move)] = neighbors filter (item => item._1.isLegal)
 
     /**
      * Returns `true` if the block is standing.
      */
-    def isStanding: Boolean = ???
+    def isStanding: Boolean = b1.col == b2.col && b1.row == b2.row
 
     /**
      * Returns `true` if the block is entirely inside the terrain.
      */
-    def isLegal: Boolean = ???
+    def isLegal: Boolean = terrain(b1) && terrain(b2)
   }
+
 }
